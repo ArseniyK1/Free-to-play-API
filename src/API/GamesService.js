e4eb19b6c96402f754f4723115e0607846f8c83b
ArseniyK1 <qwertototo2332@gmail.com>
2023-08-25 14:06:35 +0300

    add cors

--- src/API/GamesService.js
+++ src/API/GamesService.js
@@ -3,10 +3,14 @@ import axios from "axios";
 export default class GamesService {
   static async getGameList() {
     try {
-      const corsProxyUrl = "https://cors-anywhere.herokuapp.com/";
-      const apiUrl = "https://www.freetogame.com/api/games";
+      const apiUrl =
+        "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/games";
 
-      const response = await axios.get(corsProxyUrl + apiUrl);
+      const response = await axios.get(apiUrl, {
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      });
       return response.data;
     } catch (error) {
       console.log(error);
